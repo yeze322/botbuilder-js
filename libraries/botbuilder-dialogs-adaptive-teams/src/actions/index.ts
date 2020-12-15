054f4ca5a7a29942ff0996f93b746f0917b8b46f
Zichuan Ma <zim@microsoft.com>
2020-12-15 15:52:09 +0800

    added package botbuilder-dialogs-adaptive-teams

---
+++ libraries/botbuilder-dialogs-adaptive-teams/src/actions/index.ts
@@ -0,0 +1,9 @@
+/**
+ * @module botbuilder-dialogs-adaptive-teams
+ */
+/**
+ * Copyright (c) Microsoft Corporation. All rights reserved.
+ * Licensed under the MIT License.
+ */
+
+export * from './getMeetingParticipant';
---
+++ libraries/botbuilder-dialogs-adaptive-teams/src/index.ts
@@ -0,0 +1,10 @@
+/**
+ * @module botbuilder-dialogs-adaptive-teams
+ */
+/**
+ * Copyright (c) Microsoft Corporation. All rights reserved.
+ * Licensed under the MIT License.
+ */
+
+export * from './actions';
+export * from './conditions';
